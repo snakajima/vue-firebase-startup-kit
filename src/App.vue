20df5547203710c7b4644aa6986e37e25d2fb81f
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-04-30 15:38:09 -0700

    b-navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
   <div id="app">
-    <b-navbar style="background:yellow">
-      <b-navbar-item>
-        <router-link to="/">Home2</router-link>
-      </b-navbar-item>
+    <b-navbar>
+      <template slot="start">
+        <b-navbar-item href="#">Home</b-navbar-item>
+        <b-navbar-item href="#">Documentation</b-navbar-item>
+        <b-navbar-dropdown label="Info">
+          <b-navbar-item href="#">About</b-navbar-item>
+          <b-navbar-item href="#">Contact</b-navbar-item>
+        </b-navbar-dropdown>
+      </template>
+
+      <template slot="end">
+        <b-navbar-item tag="div">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a class="button is-light">Log in</a>
+          </div>
+        </b-navbar-item>
+      </template>
     </b-navbar>
     <div id="nav">
       <router-link to="/">Home2</router-link>|
