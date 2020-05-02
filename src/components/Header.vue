7b0658af3210a8c190bdc9b1be44d76c6d5e1e0a
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-02 14:47:02 -0700

    brand

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,8 @@
 <template>
   <b-navbar type="is-primary">
+    <template slot="brand">
+      <b-navbar-item tag="router-link" to="/">Firebase Startup Kit</b-navbar-item>
+    </template>
     <template slot="start">
       <b-navbar-item tag="router-link" to="/">Home</b-navbar-item>
       <b-navbar-item tag="router-link" to="/about">About</b-navbar-item>
