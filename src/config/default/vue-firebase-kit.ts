108fef9ffa00d448e3b965d0b57debb1c8ad9236
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-02 14:52:16 -0700

    firebase config

---
+++ src/config/default/vue-firebase-kit.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+	apiKey: "AIzaSyDz2zsrTAb3Tq8-2qxvdMf0t2kkCaqpuK0",
+	authDomain: "vue-firebase-kit.firebaseapp.com",
+	databaseURL: "https://vue-firebase-kit.firebaseio.com",
+	projectId: "vue-firebase-kit",
+	storageBucket: "vue-firebase-kit.appspot.com",
+	messagingSenderId: "267570366791",
+	appId: "1:267570366791:web:cb1a0179314fd9da3ee462"
+};
