8f051bd8ab07f4f654c76994833cabeb8f6f2bc9
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-02 14:59:23 -0700

    firebase.ts

---
+++ src/scripts/firebase.ts
@@ -0,0 +1,17 @@
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/storage";
+import "firebase/auth";
+import "firebase/functions";
+
+import { firebaseConfig } from "@/config/project";
+
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
+export const storage = firebase.storage();
+export const auth = firebase.auth();
+export const functions = firebase.functions();
+export const authObject = firebase.auth;
+export const firestore = firebase.firestore;
+
