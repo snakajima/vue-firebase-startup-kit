84aaebf1e8d03e00701fa88470ac6683a3efc168
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-02 19:20:28 -0700

    name

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,25 @@
 <template>
   <section class="section">
     <div class="container">
-      <h1 class="title">Hello</h1>
+      <h1 class="title">
+        Hello
+        {{userName}}
+      </h1>
       <p>Firebase rocks!</p>
     </div>
   </section>
 </template>
 
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  computed: {
+    userName() {
+      if (this.$store.state.user) {
+        return this.$store.state.user.displayName;
+      }
+      return "";
+    }
+  }
+});
+</script>
