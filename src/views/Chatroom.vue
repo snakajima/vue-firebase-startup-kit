05e46d8da9fbb10e118af49e469678316328602f
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-04 16:32:01 -0700

    messages

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -17,12 +17,23 @@ import { Unsubscribe } from "firebase";
 
 @Component
 export default class Chatroom extends Vue {
+  detacher: Unsubscribe | undefined = undefined;
   refChatroom: firebase.firestore.DocumentReference | undefined = undefined;
   chatroom: any = null;
   async mounted() {
     console.log("mounted", this.$route.params.roomId);
     this.refChatroom = db.doc(`chatrooms/${this.$route.params.roomId}`);
     this.chatroom = (await this.refChatroom.get()).data();
+    this.detacher = this.refChatroom
+      .collection("messages")
+      .onSnapshot(snapshot => {
+        console.log(snapshot);
+      });
+  }
+  destroyed() {
+    if (this.detacher) {
+      this.detacher();
+    }
   }
 }
 </script>
