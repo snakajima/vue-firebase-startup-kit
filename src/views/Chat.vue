302b676f6a9e062781ed55f83431c11984b72ff8
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-04 15:32:20 -0700

    Create Chat Room

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -2,7 +2,27 @@
   <section class="section">
     <div class="container">
       <h1 class="title">Chat</h1>
-      <p>Firebase rocks!</p>
+      <b-button @click="handleCreate">Create a Chat Room</b-button>
     </div>
   </section>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import { db, firestore } from "@/scripts/firebase";
+
+@Component
+export default class Chat extends Vue {
+  async handleCreate() {
+    console.log("handleCreate");
+    const doc = await db.collection(`chatrooms`).add({
+      owner: this.$store.state.user.uid,
+      ownerName: this.$store.state.user.displayName,
+      timeCreated: firestore.FieldValue.serverTimestamp(),
+      title: "(untitled)"
+    });
+    console.log("doc", doc);
+  }
+}
+</script>
