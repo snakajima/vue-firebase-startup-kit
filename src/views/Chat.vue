05be294a31a38311f222c2558edfba090559995b
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-04 15:46:57 -0700

    chat rooms

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -4,6 +4,9 @@
       <h1 class="title">Chat</h1>
       <b-button @click="handleCreate">Create a Chat Room</b-button>
     </div>
+    <ul>
+      <li v-for="room in chatrooms" :key="room.id">{{room.title}}</li>
+    </ul>
   </section>
 </template>
 
@@ -11,12 +14,33 @@
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
 import { db, firestore } from "@/scripts/firebase";
+import { Unsubscribe } from "firebase";
 
 @Component
 export default class Chat extends Vue {
+  detacher: Unsubscribe | undefined = undefined;
+  refChatrooms = db.collection(`chatrooms`);
+  chatrooms: Array<any> = [];
+  created() {
+    this.detacher = this.refChatrooms
+      .orderBy("timeCreated", "desc")
+      .onSnapshot(snapshot => {
+        console.log(snapshot);
+        this.chatrooms = snapshot.docs.map(doc => {
+          const data = doc.data();
+          data.id = doc.id;
+          return data;
+        });
+      });
+  }
+  destroyed() {
+    if (this.detacher) {
+      this.detacher();
+    }
+  }
   async handleCreate() {
     console.log("handleCreate");
-    const doc = await db.collection(`chatrooms`).add({
+    const doc = await this.refChatrooms.add({
       owner: this.$store.state.user.uid,
       ownerName: this.$store.state.user.displayName,
       timeCreated: firestore.FieldValue.serverTimestamp(),
