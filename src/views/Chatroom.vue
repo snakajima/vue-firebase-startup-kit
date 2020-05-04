00c00f5fc5e91f73b80dddf37d284d158d28deca
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-04 16:53:16 -0700

    messages

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -5,6 +5,16 @@
         Chatroom
         <span v-if="chatroom">: {{ chatroom.title }}</span>
       </h1>
+      <div>
+        <div
+          v-for="message in messages"
+          :key="message.id"
+        >{{message.ownerName}}: {{message.message}}</div>
+      </div>
+      <b-field label="Message">
+        <b-input v-model="message" maxlength="200" type="textarea"></b-input>
+      </b-field>
+      <b-button @click="handlePost">Post</b-button>
     </div>
   </section>
 </template>
@@ -17,6 +27,8 @@ import { Unsubscribe } from "firebase";
 
 @Component
 export default class Chatroom extends Vue {
+  message = "";
+  messages: Array<any> = [];
   detacher: Unsubscribe | undefined = undefined;
   refChatroom: firebase.firestore.DocumentReference | undefined = undefined;
   chatroom: any = null;
@@ -26,14 +38,29 @@ export default class Chatroom extends Vue {
     this.chatroom = (await this.refChatroom.get()).data();
     this.detacher = this.refChatroom
       .collection("messages")
+      .orderBy("timeCreated")
       .onSnapshot(snapshot => {
         console.log(snapshot);
+        this.messages = snapshot.docs.map(doc => {
+          const data = doc.data();
+          data.id = doc.id;
+          return data;
+        });
       });
   }
   destroyed() {
     if (this.detacher) {
       this.detacher();
     }
+  }
+  async handlePost() {
+    await this.refChatroom!.collection("messages").add({
+      owner: this.$store.state.user.uid,
+      ownerName: this.$store.state.user.displayName,
+      timeCreated: firestore.FieldValue.serverTimestamp(),
+      message: this.message
+    });
+    this.message = "";
   }
 }
 </script>
