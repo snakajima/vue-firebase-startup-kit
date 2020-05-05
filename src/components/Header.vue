6de906e28601d2c664c9a75d6cc9ede25951d2aa
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-04 19:33:17 -0700

    title change

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <b-navbar type="is-primary" :is-active.sync="isActive">
     <template slot="brand">
-      <b-navbar-item tag="router-link" to="/">Firebase Startup Kit</b-navbar-item>
+      <b-navbar-item tag="router-link" to="/">Vur+Firebase Playground</b-navbar-item>
     </template>
     <template slot="start">
       <b-navbar-item tag="router-link" to="/">Home</b-navbar-item>
