c48337cf47f4fd78d41f24fc5a3eab9e06bd6ea4
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-06 10:06:45 -0700

    cass

--- src/components/RichTextEditor.vue
+++ src/components/RichTextEditor.vue
@@ -1,17 +1,35 @@
 <template>
   <div>
-    <div v-if="false">
+    <div>
       <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
         <button :class="{ 'is-active': isActive.bold() }" @click="commands.bold">Bold</button>
       </editor-menu-bar>
-      <editor-content :editor="editor" />
+      <editor-content class="editor-box" :editor="editor" />
     </div>
-    <editor-content :editor="editor" />
   </div>
 </template>
 
 <script>
 import { Editor, EditorContent, EditorMenuBar } from "tiptap";
+import {
+  Blockquote,
+  CodeBlock,
+  HardBreak,
+  Heading,
+  OrderedList,
+  BulletList,
+  ListItem,
+  TodoItem,
+  TodoList,
+  Bold,
+  Code,
+  Italic,
+  Link,
+  Strike,
+  Underline,
+  History
+} from "tiptap-extensions";
+
 export default {
   components: {
     EditorContent,
@@ -24,6 +42,24 @@ export default {
   },
   mounted() {
     this.editor = new Editor({
+      extensions: [
+        new Blockquote(),
+        new CodeBlock(),
+        new HardBreak(),
+        new Heading({ levels: [1, 2, 3] }),
+        new BulletList(),
+        new OrderedList(),
+        new ListItem(),
+        new TodoItem(),
+        new TodoList(),
+        new Bold(),
+        new Code(),
+        new Italic(),
+        new Link(),
+        new Strike(),
+        new Underline(),
+        new History()
+      ],
       content: "<p>This is just a boring paragraph</p><p>Foo</p>",
       onUpdate: this.handleUpdate
     });
@@ -38,3 +74,9 @@ export default {
   }
 };
 </script>
+
+<style>
+.ProseMirror:focus {
+  outline: 1px #dddddd solid;
+}
+</style>
