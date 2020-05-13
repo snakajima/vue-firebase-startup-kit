6eeb914d58e5bda7c36dc9bccd2e942a854246bb
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-13 12:58:03 -0700

    chat channels

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="section">
     <h1 class="title">Blog</h1>
-    <create-new :refCollection="refArticles" @created="handleCreated" />
+    <create-new :refCollection="refCollection" @created="handleCreated" />
     <hr />
     <h2>Your Blog Articles</h2>
-    <list-view :refCollection="refArticles" path="blog" />
+    <list-view :refCollection="refCollection" path="blog" />
     <source-link path="views/Blog.vue" />
   </section>
 </template>
@@ -25,7 +25,7 @@ import SourceLink from "@/components/SourceLink.vue";
   }
 })
 export default class Blog extends Vue {
-  refArticles = db.collection(`articles`);
+  refCollection = db.collection(`articles`);
 
   handleCreated(id: string) {
     this.$router.push(`/blog/${id}?edit=1`);
--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,58 +1,34 @@
 <template>
   <section class="section">
-    <div class="container">
-      <h1 class="title">Chat</h1>
-      <b-field label="New Chat Channel">
-        <b-input v-model="title"></b-input>
-      </b-field>
-      <b-button @click="handleCreate">Create</b-button>
-    </div>
+    <h1 class="title">Chat</h1>
+    <create-new :refCollection="refCollection" @created="handleCreated" />
     <hr />
-    <h2>Your Channels</h2>
-    <ul>
-      <li v-for="room in chatrooms" :key="room.id">
-        <router-link :to="`/chat/${room.id}`">{{room.title}}</router-link>
-      </li>
-    </ul>
+    <h2>Your Chat Channels</h2>
+    <list-view :refCollection="refCollection" path="chat" />
+    <source-link path="views/Chat.vue" />
   </section>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
-import { db, firestore } from "@/scripts/firebase";
-import { Unsubscribe } from "firebase";
-import { ChatRoom } from "@/scripts/datatypes";
+import { db } from "@/scripts/firebase";
+import CreateNew from "@/components/CreateNew.vue";
+import ListView from "@/components/ListView.vue";
+import SourceLink from "@/components/SourceLink.vue";
 
-@Component
-export default class Chat extends Vue {
-  title = "";
-  detacher?: Unsubscribe;
-  refChatrooms = db.collection(`chatrooms`);
-  chatrooms: Array<ChatRoom> = [];
-
-  created() {
-    this.detacher = this.refChatrooms
-      .orderBy("timeCreated", "desc")
-      .onSnapshot(snapshot => {
-        this.chatrooms = snapshot.docs.map(doc => {
-          return Object.assign(doc.data(), { id: doc.id }) as ChatRoom;
-        });
-      });
+@Component({
+  components: {
+    SourceLink,
+    CreateNew,
+    ListView
   }
-  destroyed() {
-    this.detacher && this.detacher();
-  }
+})
+export default class Blog extends Vue {
+  refCollection = db.collection(`chatrooms`);
 
-  async handleCreate() {
-    const doc = await this.refChatrooms.add({
-      owner: this.$store.state.user.uid,
-      ownerName: this.$store.state.user.displayName,
-      timeCreated: firestore.FieldValue.serverTimestamp(),
-      title: this.title
-    });
-    console.log("doc", doc);
-    this.$router.push(`/chat/${doc.id}`);
+  handleCreated(id: string) {
+    this.$router.push(`/chat/${id}?edit=1`);
   }
 }
 </script>
