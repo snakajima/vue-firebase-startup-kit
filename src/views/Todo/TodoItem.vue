11c278be2803bde410dbf0704ee873de07b57d69
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-21 07:12:59 -0700

    UI changes

--- src/views/Todo/TodoItem.vue
+++ src/views/Todo/TodoItem.vue
@@ -1,9 +1,12 @@
 <template>
   <b-modal :active.sync="isVisible" :width="488">
     <div class="popup">
-      <b-input v-model="title" />
-      <b-button @click="handleSave">Save</b-button>
-      <b-button @click="handleDelete">Delete</b-button>
+      <b-input class="m-b-8" v-model="title" />
+      <b-button type="is-primary" :disabled="!modified" @click="handleSave">Save</b-button>
+      <b-button class="m-l-8" @click="handleCancel">Cancel</b-button>
+      <b-button class="m-l-8" type="is-danger" @click="handleDelete">
+        <i class="fas fa-trash" />
+      </b-button>
     </div>
   </b-modal>
 </template>
@@ -23,11 +26,17 @@ export default class TodoItemView extends Vue {
     this.isVisible = true;
     this.title = newValue.title;
   }
+  get modified(): boolean {
+    return this.title !== this.item.title;
+  }
 
   handleSave() {
     this.item.ref.update({
       title: this.title
     });
+    this.isVisible = false;
+  }
+  handleCancel() {
     this.isVisible = false;
   }
   handleDelete() {
--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -4,7 +4,7 @@
       <editable-title :document="todolist" :refDocument="refTodoList" @deleted="handleListDelete" />
       <div v-if="hasUser" class="m-b-8">
         <b-input v-model="newItem" class="m-b-4"></b-input>
-        <b-button type="is-primary" @click="handlePost">Post</b-button>
+        <b-button type="is-primary" @click="handlePost">Add</b-button>
       </div>
       <div v-for="item in todoitems" :key="item.id">
         <i
